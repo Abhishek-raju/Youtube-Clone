693398d63584732b634e42117eb3567ff64b89f0
Abhishek <cool.abhishekerketta@gmail.com>
2023-04-10 12:02:46 +0530

    Seacch Added

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import Header from "./components/Header"
 import LeftNav from "./components/LeftNav"
 import leftNavMenuItem from "./components/LeftNavMenuItem"
 import SearchResult from "./components/SearchResult"
-import SearchResultVideo from "./components/SearchResultVideo"
-import SuggestionVideo from "./components/SuggestionVideo"
+import SearchResultVideo from "./components/SearchResultVideoCard"
+import SuggestionVideo from "./components/SuggestionVideoCard"
 import VideoCard from "./components/VideoCard"
 import VideoDetails from "./components/VideoDetails"
 
@@ -16,13 +16,14 @@ import VideoDetails from "./components/VideoDetails"
 
 const App = () => {
     return (
+
        <AppContext>
         <BrowserRouter>
           <div className="flex flex-col">
             <Header />
             <Routes>
               <Route path="/" element={<Feed />}/>
-              <Route path="/SearchResults/:searchQuery" element={<SearchResult />}/>
+              <Route path="/SearchResult/:searchQuery" element={<SearchResult />}/>
               <Route path="/video/:id" element={<VideoDetails/>}/>
             </Routes>
           </div>
